9c6f98d98bf5c6ebca17d9fa516d451bfba9bc66
José Ivan <jjivanc12@gmail.com>
2022-05-23 14:58:56 -0300

    add funcao botao

--- main.js
+++ main.js
@@ -11,19 +11,25 @@ const page02 = document.querySelector('#page02');
 const page03 = document.querySelector('#page03');
 const letras = document.querySelector('.letras')
 const palavra = document.querySelector('#palavra')
+const novaPalavra = document.querySelector('#addNew')
 //--------------------------
-const words = ['mundoo']
-var sordeado = words[0]
-comecar.addEventListener('click', ()=> {
+const words = ['mundo','rato']
+var contador = 1
+var sorteado = ''
+function play(){
+    sorteador()
+    separador = sorteado.split('');
     console.log('comecar');
     page01.classList.add('hide');
     page02.classList.remove('hide');
-    var separador = sordeado.split('');
+    
     for (var i = 0; i < separador.length; i++ ){
-        console.log(i)
+        //console.log(i)
         var div = document.createElement('div');
         var img = document.createElement('img');
         var p = document.createElement('p');
+        p.classList.add('visibility');
+        p.setAttribute('id',`letra-${i}`)
         p.textContent = separador[i]
         img.src = './images/Letter3.png';
         letras.appendChild(div)
@@ -31,8 +37,37 @@ comecar.addEventListener('click', ()=> {
         div.appendChild(img)
     };
     console.log(separador);
-    
+};
+comecar.addEventListener('click', play);
+document.addEventListener('keydown', (event) =>{
+    const keyName = event.key;
+    comparador(keyName);
 });
+function comparador(item){
+    var acertou = false;
+    for(var i=0;i<separador.length;i++){
+        if(separador[i] == item){
+            var letra = letras.querySelector('#letra-'+i);
+            letra.classList.remove('visibility');
+            acertou = true
+            console.log('letra certa');
+        }else{
+            /*var stick = document.querySelector('#stick')
+            stick.src = `./images/stick-man${contador}.svg`
+            contador++
+            console.log(contador)*/
+        }
+    };
+    if(!acertou){
+        console.log('errou')
+        var stick = document.querySelector('#stick')
+        stick.src = `./images/stick-man${contador}.svg`
+        contador++
+    }
+    if(contador > 6){
+        alert('perdeu Playboy')
+    };
+};
 adicionar.addEventListener('click', ()=> {
     console.log('adicionar');
     page01.classList.add('hide');
@@ -40,6 +75,11 @@ adicionar.addEventListener('click', ()=> {
 });
 novo.addEventListener('click', ()=> {
     console.log('novo');
+    letras.innerHTML = ''
+    var stick = document.querySelector('#stick')
+    stick.src = `./images/stick-man0.svg`
+    contador = 1
+    play();
 });
 desistir.addEventListener('click', ()=> {
     console.log('desistir');
@@ -50,8 +90,18 @@ cancelar.addEventListener('click', ()=> {
     page01.classList.remove('hide');
 });
 salvar.addEventListener('click', ()=> {
+    var adicionarPalavra = novaPalavra.value
+    words.push(adicionarPalavra)
+    console.log(adicionarPalavra)
     console.log('salvar');
+    play();
+    page03.classList.add('hide');
 });
+function sorteador(){
+    var numero = Math.floor(Math.random() * words.length);
+    sorteado = words[numero]
+    console.log(numero)
+};
 
 
 console.log('ativo');
